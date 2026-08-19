9703b7db274bd7ca9584b885eb5ac351df243a1a
shadha <shadharashid2001@gmail.com>
2026-08-19 14:39:26 +0400

    feat(entities): Appointment class + fields   [Task 1.6]

--- src/entities/Appointment.java
+++ src/entities/Appointment.java
@@ -1,4 +1,97 @@
 package entities;
 
 public class Appointment {
+   private String appointmentId;
+   private String patientId;
+   private String doctorId;
+   private String  appointmentDate;
+   private String appointmentTime;
+   private String status;
+   private String reason;
+   private boolean isFollowUp;
+
+   public Appointment
+       ( String appointmentId,
+         String patientId,
+         String doctorId,
+         String  appointmentDate,
+         String appointmentTime,
+         String status,
+         String reason,
+         boolean isFollowUp
+               ){
+
+       setAppointmentId(appointmentId);
+       setPatientId(patientId);
+       setDoctorId(doctorId);
+       setAppointmentDate(appointmentDate);
+       setAppointmentTime(appointmentTime);
+       setStatus(status);
+       setReason(reason);
+       setFollowUp(isFollowUp);
+   }
+
+    public String getAppointmentId() {
+        return appointmentId;
+    }
+
+    public void setAppointmentId(String appointmentId) {
+        this.appointmentId = appointmentId;
+    }
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(String doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(String appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public boolean isFollowUp() {
+        return isFollowUp;
+    }
+
+    public void setFollowUp(boolean followUp) {
+        isFollowUp = followUp;
+    }
 }
