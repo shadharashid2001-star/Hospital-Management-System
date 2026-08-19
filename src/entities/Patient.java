c10a4712f5f089c11ac35d602d27599350c6f4b9
shadha <shadharashid2001@gmail.com>
2026-08-19 09:50:15 +0400

    feat(entities): Patient override displayInfo

--- src/entities/Patient.java
+++ src/entities/Patient.java
@@ -47,4 +47,99 @@ public class Patient extends Person {
         this.bloodGroup = bloodGroup;
         this.emergencyContact = emergencyContact;
     }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public String getEmergencyContact() {
+        return emergencyContact;
+    }
+
+    public void setEmergencyContact(String emergencyContact) {
+        this.emergencyContact = emergencyContact;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String[] getAllergies() {
+        return allergies;
+    }
+
+    public void setAllergies(String[] allergies) {
+        this.allergies = allergies;
+    }
+
+    public int getAllergyCount() {
+        return allergyCount;
+    }
+
+    public void setAllergyCount(int allergyCount) {
+        this.allergyCount = allergyCount;
+    }
+
+    public String[] getRecordIds() {
+        return recordIds;
+    }
+
+    public void setRecordIds(String[] recordIds) {
+        this.recordIds = recordIds;
+    }
+
+    public int getRecordCount() {
+        return recordCount;
+    }
+
+    public void setRecordCount(int recordCount) {
+        this.recordCount = recordCount;
+    }
+
+    public double getOutstandingBalance() {
+        return outstandingBalance;
+    }
+
+    public void setOutstandingBalance(double outstandingBalance) {
+        this.outstandingBalance = outstandingBalance;
+    }
+
+    public boolean isInsured() {
+        return insured;
+    }
+
+    public void setInsured(boolean insured) {
+        this.insured = insured;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println(
+                "Blood Group: " + bloodGroup);
+
+        System.out.println(
+                "Emergency Contact: "
+                        + emergencyContact);
+
+        System.out.println(
+                "Registration Date: "
+                        + registrationDate);
+
+        System.out.println(
+                "Outstanding Balance: "
+                        + outstandingBalance);
+
+        System.out.println(
+                "Insured: " + insured);
+    }
+    }
 }
