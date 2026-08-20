8ca7e7d3a5356a7b089b14b40e1cd69d62e89fba
shadha <shadharashid2001@gmail.com>
2026-08-20 07:39:42 +0400

    feat(services): addPatient overloads (x3)   [Task 2.2]

--- src/services/PatientService.java
+++ src/services/PatientService.java
@@ -1,4 +1,83 @@
 package services;
 
+import entities.Patient;
+import java.util.ArrayList;
+
 public class PatientService {
+
+    private ArrayList<Patient> patients = new ArrayList<>();
+
+
+
+    public Patient addPatient(
+            String id,
+            String firstName,
+            String lastName) {
+
+        Patient patient = new Patient(
+                id,
+                firstName,
+                lastName,
+                "2001-01-29",
+                "Unknown",
+                "00000000",
+                "",
+                "",
+                id,
+                0,
+                true,
+                "Unknown",
+                "",
+                "2026-01-01",
+                0,
+                false
+        );
+
+        patients.add(patient);
+
+        return patient;
+    }
+
+    public Patient addPatient(
+            String id,
+            String firstName,
+            String lastName,
+            String bloodGroup) {
+
+        Patient patient = new Patient(
+                id,
+                firstName,
+                lastName,
+                "2000-01-01",
+                "Unknown",
+                "00000000",
+                "",
+                "",
+                id,
+                0,
+                true,
+                bloodGroup,
+                "",
+                "2026-01-01",
+                0,
+                false
+        );
+
+        patients.add(patient);
+
+        return patient;
+    }
+
+
+    public Patient addPatient(Patient patient) {
+
+        patients.add(patient);
+
+        return patient;
+    }
+
+
+    public ArrayList<Patient> getPatients() {
+        return patients;
+    }
 }
