7aa0909afd9acc5d477d1ea89cf0e193b0c80521
shadha <shadharashid2001@gmail.com>
2026-08-20 07:58:26 +0400

    feat(main): findOldest walk   [Task 2.3]

--- src/main/HospitalApp.java
+++ src/main/HospitalApp.java
@@ -1,7 +1,4 @@
 import entities.Person;
-
-import java.util.ArrayList;
-import entities.Person;
 import entities.Patient;
 import entities.Doctor;
 import entities.Nurse;
@@ -10,43 +7,90 @@ import entities.Surgeon;
 
 import java.util.ArrayList;
 
-public void countByType(ArrayList<Person> people) {
+public class HospitalApp {
 
-    Integer patientCount = 0;
-    Integer doctorCount = 0;
-    Integer nurseCount = 0;
-    Integer inPatientCount = 0;
-    Integer surgeonCount = 0;
+    public void countByType(ArrayList<Person> people) {
 
-    for (Person person : people) {
+        Integer patientCount = 0;
+        Integer doctorCount = 0;
+        Integer nurseCount = 0;
+        Integer inPatientCount = 0;
+        Integer surgeonCount = 0;
 
-        if (person instanceof InPatient) {
+        for (Person person : people) {
 
-            inPatientCount++;
-            patientCount++;
+            if (person instanceof InPatient) {
 
-        } else if (person instanceof Surgeon) {
+                inPatientCount++;
+                patientCount++;
 
-            surgeonCount++;
-            doctorCount++;
+            } else if (person instanceof Surgeon) {
 
-        } else if (person instanceof Patient) {
+                surgeonCount++;
+                doctorCount++;
 
-            patientCount++;
+            } else if (person instanceof Patient) {
 
-        } else if (person instanceof Doctor) {
+                patientCount++;
 
-            doctorCount++;
+            } else if (person instanceof Doctor) {
 
-        } else if (person instanceof Nurse) {
+                doctorCount++;
 
-            nurseCount++;
+            } else if (person instanceof Nurse) {
+
+                nurseCount++;
+            }
         }
+
+        System.out.println("Patients: " + patientCount);
+        System.out.println("InPatients: " + inPatientCount);
+        System.out.println("Doctors: " + doctorCount);
+        System.out.println("Surgeons: " + surgeonCount);
+        System.out.println("Nurses: " + nurseCount);
     }
 
-    System.out.println("Patients: " + patientCount);
-    System.out.println("InPatients: " + inPatientCount);
-    System.out.println("Doctors: " + doctorCount);
-    System.out.println("Surgeons: " + surgeonCount);
-    System.out.println("Nurses: " + nurseCount);
+
+    public Person findOldest(ArrayList<Person> people) {
+
+        Person oldest = null;
+
+        for (Person person : people) {
+
+            if (oldest == null
+                    || person.getAge() > oldest.getAge()) {
+
+                oldest = person;
+            }
+        }
+
+        return oldest;
+    }
+
+
+    public static void main(String[] args) {
+
+        HospitalApp app = new HospitalApp();
+
+        ArrayList<Person> people = new ArrayList<>();
+        // people.add(patient);
+        // people.add(doctor);
+        // people.add(nurse);
+        // people.add(inPatient);
+        // people.add(surgeon);
+
+        app.countByType(people);
+
+        Person oldest = app.findOldest(people);
+
+        if (oldest != null) {
+
+            System.out.println(
+                    "Oldest Person: "
+                            + oldest.getFullName()
+                            + " - "
+                            + oldest.getAge()
+            );
+        }
+    }
 }
